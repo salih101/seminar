47942f46faf5e8a933c36759ddc99357c933818a
salih101 <95134518+salih101@users.noreply.github.com>
2022-11-14 07:17:15 +0530

    1st commit

---
+++ n_linked_list.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+}*head,*newNode,*temp;
+
+int main(){
+    int n,i;
+    printf("Enter the Number of Nodes to Create:");
+    scanf("%d",&n);
+
+    for (i=1;i<=n;i++){
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        printf("Enter Data for Node %d:",i);
+        scanf("%d",&newNode->data);
+        newNode ->next = NULL;
+        if (head == NULL){
+            head = temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    printf("List Data:\n");
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    
+}
+
+
